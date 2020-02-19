c2e6b6cd0a31cb88901f01175a1c977eae9ff6a4
Alexey Berezin <beraliv.spb@gmail.com>
2020-02-20 00:12:17 +0300

    ts: ➕ add Invert

---
+++ Invert/index.spec.ts
@@ -0,0 +1,53 @@
+import { Invert } from './'
+
+// ✅ It is working with string | number | symbol
+const two = Symbol('2');
+type Number = {
+  1: 'one'
+  [two]: 'two'
+  3: 'three'
+  4: 'four'
+  5: 'five'
+  6: 'six'
+  7: 'seven'
+  8: 'eight'
+  9: 'nine'
+}
+// { two: typeof two, one: 1; three: 3; four: 4; five: 5; six: 6; seven: 7; eight: 8; nine: 9; }
+type InvertedNumber = Invert<Number>;
+
+// ❌ It cannot work with object and array
+type Flat = {
+  rooms: {
+    name: string
+    area: number
+  }[]
+  tenant?: {
+    name: string
+  }
+}
+// @ts-ignore
+type InvertedFlat = Invert<Flat>
+
+// ✅ It merges the same values and gets union types
+type FamilyMember = {
+  dad: 'Andrew'
+  mum: 'Arina'
+  brother: 'Andrew'
+}
+// { Andrew: "dad" | "brother"; Arina: "mum"; }
+type InvertedFamilyMember = Invert<FamilyMember>;
+
+// ✅ It ignores `null` and `undefined`
+type Box = {
+  pens: null
+  pencils: undefined
+}
+// {}
+type InvertedBox = Invert<Box>;
+// {}
+type DoubleInvertedBox = Invert<Invert<Box>>;
+
+// ✅ It gets type as is after double usage
+// { dad: 'Andrew'; mum: 'Arina'; brother: 'Andrew'; }
+type DoubleInvertedFamilyMember = Invert<Invert<FamilyMember>>;
---
+++ Invert/index.ts
@@ -0,0 +1,13 @@
+type InvertibleObject = Record<PropertyKey, PropertyKey>
+
+type KeyByValue<V, O extends InvertibleObject> = {
+  // Use `V extends O[K]` instead of `O[K] extends V` to ignore `null` and `undefined`
+  [K in keyof O]: V extends O[K] ? K : never
+}[keyof O]
+
+/**
+ * Inverts object's keys and values
+ */
+export type Invert<O extends InvertibleObject> = {
+  [Value in O[keyof O]]: KeyByValue<Value, O>
+}
