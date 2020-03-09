406d9849b0729efb86ab50a2b12221d7d50ee3d7
Alexey Berezin <beraliv.spb@gmail.com>
2020-03-09 13:07:41 +0300

    ts: 🔄 update examples

--- CustomTypes/Mark/BooleanMark.spec.ts
+++ CustomTypes/Mark/BooleanMark.spec.ts
@@ -1,12 +1,17 @@
 import { BooleanMark } from './BooleanMark'
 
-// ✅ It adds value `true` with a specified key
+// ✅ It adds value `true` by default with a specified key
 type Distance = number & BooleanMark<'distance'>
 type Time = number & BooleanMark<'time'>
 
+// ✅ It accepts `false` as value with a specified key
+type RelativeValue = number & BooleanMark<'absolute', false>
+
 let sec = 1 as Time
 let km = 1 as Distance
+let ratio = 1 as RelativeValue
 
 // ✅ Flags are accessible only in types but return undefined
 sec.time
 km.distance
+ratio.absolute
