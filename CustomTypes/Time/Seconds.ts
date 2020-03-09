edf77e2a6d287f9d1826c01ae03db47dc2827929
Alexey Berezin <beraliv.spb@gmail.com>
2020-03-09 13:22:04 +0300

    ts: 🔄 add boolean mark for Seconds to distinguish it from Minutes

--- CustomTypes/Time/Seconds.ts
+++ CustomTypes/Time/Seconds.ts
@@ -1,6 +1,7 @@
 import { AbsoluteValue } from "../Mark/AbsoluteValue"
+import { BooleanMark } from "../Mark/BooleanMark"
 
 /**
  * Declares time in seconds
  */
-export type Seconds = number & AbsoluteValue
+export type Seconds = number & AbsoluteValue & BooleanMark<'seconds'>
